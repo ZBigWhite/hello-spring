7cc50881b087c2ce3221ca99e87f59cfa55955c6
ZBigWhite <946473603@qq.com>
2021-08-04 22:28:48 +0800

    线程池

--- src/main/java/geektime/learn/HelloWorld.java
+++ src/main/java/geektime/learn/HelloWorld.java
@@ -10,6 +10,11 @@
  */
 package geektime.learn;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 功能描述:<br>
  *
@@ -18,8 +23,40 @@ package geektime.learn;
  * @since [产品/模块版本] （可选）
  */
 public class HelloWorld {
-    public static void main(String[] args) {
-        String s = "哈哈哈哈";
-        System.out.println(s);
+    public static class MyTask1 implements Runnable{
+        private int i;
+
+        MyTask1(int i){
+            this.i=i;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("任务跑到" + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+    public static  void main(String args[]){
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(50, 50, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                System.out.println("创建线程"+t);
+                return  t;
+            }
+        });
+
+        for (int i = 0;i<=10;i++){
+            threadPoolExecutor.submit(new MyTask1(i));
+        }
+        threadPoolExecutor.shutdown();
+    }
+
 }
