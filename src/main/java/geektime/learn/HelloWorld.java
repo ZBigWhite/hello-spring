423e30c08c7b91cb9f5c63bfa15f9d115436fe55
ZBigWhite <946473603@qq.com>
2021-08-04 21:04:09 +0800

    Initial commit

---
+++ src/main/java/geektime/learn/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C), 2002-2021, 苏宁易购电子商务有限公司
+ * FileName: HelloWorld.java
+ * Author:   20018848
+ * Date:     2021/8/4 20:33
+ * Description: //模块目的、功能描述
+ * History: //修改记录
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名    修改时间    版本号       描述
+ */
+package geektime.learn;
+
+/**
+ * 功能描述:<br>
+ *
+ * @author 20018848
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s = "哈哈哈哈";
+        System.out.println(s);
+    }
+}
---
+++ src/main/java/geektime/spring/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,16 @@
+package geektime.spring.hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 94647
+ */
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
